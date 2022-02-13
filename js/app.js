6234f4851317c5ac43eb9c51cce7df0584f3707f
Shidul Islam <sishahin093@gmail.com>
2022-02-13 15:01:15 +0600

    28-6 Use event bubble to create calculator and clear

--- js/app.js
+++ js/app.js
@@ -12,3 +12,18 @@ function generatePin() {
     document.getElementById('display-pin').value = pin;
     // console.log(pin);
 }
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = null;
+        }
+
+    } else {
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+})
